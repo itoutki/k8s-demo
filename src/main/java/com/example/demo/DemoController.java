cfad9abda82818c10940064068e83e880dcb5c95
itoutki <itoutki@nttdata.co.jp>
2019-06-03 15:00:33 +0900

    update

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,16 +1,31 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DemoController {
+    private String podName;
+    private String version;
+
+    public DemoController(@Value("${my.label.version:None}")String version,
+                          @Value("${my.pod.name:None}") String podName) {
+        this.version = version;
+        this.podName = podName;
+    }
 
     @GetMapping("/version")
     @ResponseBody
     public String getVersion() {
-        return "{\"version\": \"v000\"}";
+        return "{\"version\": \"" + version + "\"}";
+    }
+
+    @GetMapping("/pod")
+    @ResponseBody
+    public String getPodName() {
+        return "{\"podname\": \"" + podName + "\"}";
     }
 
     @GetMapping("/")
